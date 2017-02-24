a6dd505cf33b76622c2d08818f79dbbfedd405e9
sandy0089 <sandymanmode89@gmail.com>
2017-02-24 12:47:50 +0530

    view properties

--- app/src/main/java/com/example/sandy/my_button/MainActivity.java
+++ app/src/main/java/com/example/sandy/my_button/MainActivity.java
@@ -17,13 +17,29 @@ public class MainActivity extends AppCompatActivity {
 
 
         Button btnDate= (Button) findViewById(R.id.btnDate);
-        btnDate.setOnClickListener(new View.OnClickListener() {
-            @Override
+       // btnDate.setOnClickListener(new View.OnClickListener() {
+           /* @Override
             public void onClick(View view) {
                 TextView txt = (TextView) findViewById(R.id.txtDate);
                 txt.setText(new Date().toString());
 
             }
-        });
+        });*/
+
+        btnDate.setOnClickListener(this::date);
+    }
+
+    private void date(View view){
+        TextView txtDate= (TextView) findViewById(R.id.txtDate);
+        txtDate.setText(new Date().toString());
+    }
+
+
+    private class Click implements View.OnClickListener {
+
+        public void onClick(View v) {
+            TextView txtDate = (TextView) findViewById(R.id.txtDate);
+            txtDate.setText(new Date().toString());
+        }
     }
 }
